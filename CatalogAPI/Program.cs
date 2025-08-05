97ef62d218a54085007667e9eb23a3cec3fd2b99
bruno correa <bruno.correa@justtraveltour.com>
2025-08-04 21:12:36 -0300

    feat: add domains, ef and migrate

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,3 +1,5 @@
+using CatalogAPI.Context;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection))); 
 
 var app = builder.Build();
 
