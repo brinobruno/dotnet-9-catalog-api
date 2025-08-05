2d1614a62def1bdf3276647ac76be8398eed5246
bruno correa <bruno.correa@justtraveltour.com>
2025-08-04 21:30:18 -0300

    feat: add data annotations for new table migrations

--- CatalogAPI/Domain/Category.cs
+++ CatalogAPI/Domain/Category.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogAPI.Domain;
 
+/*
+ * Many redundants data annotations ahead - doing for the sake of practice.
+ */
+
+[Table("Categories")]
 public class Category
 {
     public Category()
     {
         Products = new Collection<Product>();
     }
+    [Key]
     public int CategoryId { get; set; }
+    
+    [Required]
+    [StringLength(80)] // Bytes
     public string? Name { get; set; }
+    
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
     
     public ICollection<Product>? Products { get; set; }
--- CatalogAPI/Domain/Product.cs
+++ CatalogAPI/Domain/Product.cs
@@ -1,13 +1,36 @@
-﻿namespace CatalogAPI.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogAPI.Domain;
+
+/*
+ * Many redundants data annotations ahead - doing for the sake of practice.
+ */
+
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+    
+    [Required]
+    [StringLength(80)] // Bytes 
     public string? Name { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(10, 2)")] // 10 digits, 2 decimal places
     public decimal Price { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
+    
     public float Stock { get; set; }
+    
     public DateTime RegistrationDate { get; set; }
     
     public int CategoryId { get; set; }
